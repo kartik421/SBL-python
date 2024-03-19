0413fbe68ad0d800d299c583b868b6a86212360d
kartik421 <140586577+kartik421@users.noreply.github.com>
2024-03-19 12:13:44 +0530

    Add files via upload

---
+++ EXP 1/main.py
@@ -0,0 +1,23 @@
+num1= int  (input("Enter the value pf 1 st no."))
+num2= int  (input("enter the value of 2nd no "))
+sum= num1+num2
+print(sum)
+
+sub= num1-num2
+print('Subtraction is ',sub)
+
+mul= num1*num2
+print('multiplication is',mul)
+
+div= num1/num2
+print('div is',div)
+
+mod= div%10
+print('mod is',mod)
+
+exp=num1**num2
+print('exp is',exp)
+
+fd=num1//num2
+print('fd is ',fd)
+
