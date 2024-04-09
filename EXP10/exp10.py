73003caaa599690f6c1ddf54effff5aa30588255
kartik421 <140586577+kartik421@users.noreply.github.com>
2024-04-09 12:22:48 +0530

    Add files via upload

---
+++ EXP10/exp10.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+# Sample data
+x = [1, 2, 3, 4, 5]
+y = [10, 15, 7, 20, 12]
+
+# Plotting the data
+plt.plot(x, y)
+
+# Adding labels and title
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Simple Graph')
+
+# Displaying the graph
+plt.show()
